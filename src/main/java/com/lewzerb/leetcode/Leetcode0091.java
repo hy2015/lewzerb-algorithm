acc8b7372bc839c4049b0269eb006e649e3957f1
echhaiy <haiyang.chen@ericsson.com>
2020-11-11 17:13:13 +0800

    Daily update 2020-11-11

---
+++ src/main/java/com/lewzerb/leetcode/Leetcode0091.java
@@ -0,0 +1,48 @@
+package com.lewzerb.leetcode;
+
+public class Leetcode0091 {
+
+    public static void main(String[] args) {
+        Object[][] tests = {
+            {"12", 2},
+            {"226", 3},
+            {"0", 0},
+            {"1123", 5},
+            {"2101", 1},
+            {"101", 1},
+            {"10", 1},
+            {"1", 1}
+        };
+        Leetcode0091 leetcode0091 = new Leetcode0091();
+        for(Object[] test : tests){
+            String s = (String) test[0];
+            int i = (int) test[1];
+            System.out.println((leetcode0091.numDecodings(s) == i) + "\t" + leetcode0091.numDecodings(s));
+        }
+    }
+
+    public int numDecodings(String s) {
+        if(s.charAt(0) == '0'){
+            return 0;
+        }
+        int[] dp = new int[s.length()+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        if(s.length() == 1) return 1;
+
+        for(int i=2; i<=s.length(); i++){
+            int pre = s.charAt(i - 2) - '0';
+            int cur = s.charAt(i - 1) - '0';
+            if((cur+pre == 0) ||
+                (cur == 0 && pre > 2)){
+                return 0;
+            }else if(pre == 0 || cur == 0){
+                dp[i] = cur== 0?dp[i-2]:dp[i-1];
+            }else {
+                int con = pre * 10 + cur;
+                dp[i] = con>26?dp[i-1]:dp[i-1]+dp[i-2];
+            }
+        }
+        return dp[s.length()];
+    }
+}
---
+++ src/main/java/com/lewzerb/leetcode/Leetcode0095.java
@@ -0,0 +1,53 @@
+package com.lewzerb.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode0095 {
+
+    public static void main(String[] args) {
+        Leetcode0095 leetcode0095 = new Leetcode0095();
+        List<TreeNode> treeNodes = leetcode0095.generateTrees(3);
+        System.out.println(treeNodes.size());
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        return dfs(1, n);
+    }
+
+    private List<TreeNode> dfs(int begin, int end){
+        List<TreeNode> res = new ArrayList<>();
+        for(int i=begin; i<=end; i++){
+
+            List<TreeNode> lefts = dfs(begin, i - 1);
+            List<TreeNode> rights = dfs(i + 1, end);
+
+            if(!lefts.isEmpty() && !rights.isEmpty()){
+                for(TreeNode left: lefts){
+                    for(TreeNode right: rights){
+                        TreeNode root = new TreeNode(i);
+                        root.left = left;
+                        root.right = right;
+                        res.add(root);
+                    }
+                }
+            }else if(!lefts.isEmpty()){
+                for(TreeNode left: lefts){
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    res.add(root);
+                }
+            }else if(!rights.isEmpty()){
+                for(TreeNode right: rights){
+                    TreeNode root = new TreeNode(i);
+                    root.right = right;
+                    res.add(root);
+                }
+            }else {
+                TreeNode root = new TreeNode(i);
+                res.add(root);
+            }
+        }
+        return res;
+    }
+}
